816fe60abcba32f07d034efacdb540b563d6d917
dhanushgubba <dhanushgubba.ap@gmail.com>
2025-08-18 20:30:30 +0530

    more updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,20 @@ import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Chats from './pages/Chats';
 import { NhostProvider } from '@nhost/react';
+import { NhostApolloProvider } from '@nhost/react-apollo';
 import { nhost } from './nhost';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'https://nqlsviljwkjxtpycwjsl.hasura.ap-south-1.nhost.run/v1/graphql', // replace with Hasura endpoint
-  cache: new InMemoryCache(),
-  headers: {
-    'x-hasura-admin-secret': '7IGS$H1fNm&JhBPQC#mYrCG351@j^ogp', // replace if needed
-  },
-});
 
 const App = () => {
   return (
-    <ApolloProvider client={client}>
-      <NhostProvider nhost={nhost}>
+    <NhostProvider nhost={nhost}>
+      <NhostApolloProvider nhost={nhost}>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/chats" element={<Chats />} />
         </Routes>
-      </NhostProvider>
-    </ApolloProvider>
+      </NhostApolloProvider>
+    </NhostProvider>
   );
 };
 
